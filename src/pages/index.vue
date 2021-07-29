b99b10583fd21b46b31ec3983b1e529c4a2ab21b
Qing <1352272648@qq.com>
2021-07-29 18:28:04 +0800

    first-comment

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index">
+    <star-score :score = '4' :maxScore = '5' :star_num = '5' >
+    </star-score>
+  </div>
+</template>
+
+<script>
+  import starScore from "../components/star-score.vue";
+  export default {
+    name: "Index",
+    components: {
+      starScore
+    },
+    methods:{
+      test:function(){
+        this.$confirm();
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
